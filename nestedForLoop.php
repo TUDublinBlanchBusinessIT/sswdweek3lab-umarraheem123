a75e81338b12104cb5439b29da5558c7345d2f9b
B00152481 Umar Raheem <B00152481@mytudublin.ie>
2025-10-01 10:41:07 +0100

    Completed part 3

---
+++ nestedForLoop.php
@@ -0,0 +1,16 @@
+<?PHP
+echo "<TABLE border='1'>"
+for ($i=0; $i<15; $i++) {
+	echo "<TR>";
+	for ($j=0; $<=20; $j++) {
+		echo "<TD>$i, $j</TD>";
+}
+echo "</TR>"
+}
+echo "</TABLE>";
+	
+
+
+
+
+?>
